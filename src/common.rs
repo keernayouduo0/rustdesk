774a10ff0a67dfa701ed30a022778ff7bef10145
keernayouduo0 <50112175+keernayouduo0@users.noreply.github.com>
2025-06-29 01:16:17 +0800

    test2

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://121.40.51.125:21114/_admin/".to_owned()
 }
 
 #[inline]
